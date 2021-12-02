6e8b202fb484594c8427021228b38a4d074dac32
Richard Meissner <msc.meissner@gmail.com>
2021-12-02 21:09:03 +0100

    Simple test

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,42 @@
 //import Ganache, { JsonRpcPayload, JsonRpcResponse } from "ganache-core"
 //import promisify from "util.promisify"
 import * as core from '@actions/core'
-import axios from 'axios'
+import { getMultiSendDeployment, getMultiSendCallOnlyDeployment, getCreateCallDeployment, getSignMessageLibDeployment } from '@gnosis.pm/safe-deployments'
+import axios, { AxiosResponse } from 'axios'
+
+const validDelegateCallTargets = [
+  getMultiSendCallOnlyDeployment()?.defaultAddress,
+  getMultiSendDeployment()?.defaultAddress,
+  getCreateCallDeployment()?.defaultAddress,
+  getSignMessageLibDeployment()?.defaultAddress,
+].filter((value) => value !== undefined)
 
 interface MultisigTransaction {
   to: string
   value: string,
   data: string,
-  operation: string
+  operation: number
 }
 
 async function run(): Promise<void> {
   try {
-    const safe: string = core.getInput('safe-address')
+    const safeAddress: string = core.getInput('safe-address')
     const serviceUrl: string = core.getInput('service-url')
     const safeTxHash: string = core.getInput('safe-tx-hash')
     //const nodeUrl: string = core.getInput('node-url')
-    const response: MultisigTransaction = await axios.get(`${serviceUrl}/api/v1/multisig-transactions/${safeTxHash}`)
-    console.log({response})
+    const response: AxiosResponse<MultisigTransaction> = await axios.get(`${serviceUrl}/api/v1/multisig-transactions/${safeTxHash}`)
+    const transaction = response.data
+    console.log({ transaction })
+    if (transaction.operation == 1) {
+      if (validDelegateCallTargets.indexOf(transaction.to) < 0) {
+        throw Error("Invalid delegatecall target")
+      }
+      core.warning("Transaction performs a delegate call")
+    }
+
+    if (transaction.to.toLowerCase() === safeAddress.toLowerCase()) {
+      core.warning("Transaction target Safe itself")
+    }
     /*
     const options: Ganache.IProviderOptions = { db_path: "/", fork: nodeUrl, gasLimit: 100000000, gasPrice: "0" }
     const provider = Ganache.provider(options)
