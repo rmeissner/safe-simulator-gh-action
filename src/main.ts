8d7e37cc918419b7c51db9feece7e7c06486d836
Richard Meissner <msc.meissner@gmail.com>
2021-12-02 21:27:23 +0100

    Simulation prototype

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-//import Ganache, { JsonRpcPayload, JsonRpcResponse } from "ganache-core"
-//import promisify from "util.promisify"
+import Ganache, { JsonRpcPayload, JsonRpcResponse } from "ganache-core"
+import promisify from "util.promisify"
 import * as core from '@actions/core'
 import { getMultiSendDeployment, getMultiSendCallOnlyDeployment, getCreateCallDeployment, getSignMessageLibDeployment } from '@gnosis.pm/safe-deployments'
 import axios, { AxiosResponse } from 'axios'
+import { ethers } from "ethers"
 
 const validDelegateCallTargets = [
   getMultiSendCallOnlyDeployment()?.defaultAddress,
@@ -12,36 +13,60 @@ const validDelegateCallTargets = [
 ].filter((value) => value !== undefined)
 
 interface MultisigTransaction {
-  to: string
+  safeTxHash: string,
+  to: string,
   value: string,
   data: string,
   operation: number
 }
+
+interface SafeInfo {
+  owners: string[]
+}
+
+const safeInterface = new ethers.utils.Interface(["function approveHash(bytes32)"])
 
 async function run(): Promise<void> {
   try {
     const safeAddress: string = core.getInput('safe-address')
     const serviceUrl: string = core.getInput('service-url')
     const safeTxHash: string = core.getInput('safe-tx-hash')
-    //const nodeUrl: string = core.getInput('node-url')
+    const nodeUrl: string = core.getInput('node-url')
     const response: AxiosResponse<MultisigTransaction> = await axios.get(`${serviceUrl}/api/v1/multisig-transactions/${safeTxHash}`)
     const transaction = response.data
     console.log({ transaction })
+
+    if (transaction.to.toLowerCase() === safeAddress.toLowerCase()) {
+      core.warning("Transaction target Safe itself")
+    }
+
+    const failOnUnknownDelegatecall: string = core.getInput('fail-on-unknown-delegatecall')
     if (transaction.operation == 1) {
-      if (validDelegateCallTargets.indexOf(transaction.to) < 0) {
+      if (failOnUnknownDelegatecall === "true" && validDelegateCallTargets.indexOf(transaction.to) < 0) {
         throw Error("Invalid delegatecall target")
       }
       core.warning("Transaction performs a delegate call")
     }
 
-    if (transaction.to.toLowerCase() === safeAddress.toLowerCase()) {
-      core.warning("Transaction target Safe itself")
+    const simulateTx: string = core.getInput('simulate-tx')
+    if (simulateTx == "true") {
+      const response: AxiosResponse<SafeInfo> = await axios.get(`${serviceUrl}/api/v1/safes/${safeAddress}`)
+      const safeInfo = response.data
+      console.log({ safeInfo })
+      const options: Ganache.IProviderOptions = { db_path: "/", fork: nodeUrl, gasLimit: 100000000, gasPrice: "0" }
+      const provider = Ganache.provider(options)
+      const execute = promisify(provider.send.bind(provider))
+      for (const owner in safeInfo.owners) {
+        await execute("evm_unlockUnknownAccount", [owner])
+        console.log(await execute("eth_sendTransaction", [{
+          to: safeAddress,
+          data: safeInterface.encodeFunctionData("approveHash", [transaction.safeTxHash]),
+          from: owner,
+          gasPrice: 0,
+          gasLimit: 10000000
+        }]))
+      }
     }
-    /*
-    const options: Ganache.IProviderOptions = { db_path: "/", fork: nodeUrl, gasLimit: 100000000, gasPrice: "0" }
-    const provider = Ganache.provider(options)
-    const execute = promisify(provider.send.bind(provider))
-    */
 
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
